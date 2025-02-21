2e0655e1f16bcfbc9c4aaaa977800b34dd7ed359
Tim <timmywilliams4665@gmail.com>
2025-02-21 12:06:01 -0800

    finished guestbook 3 assingment

--- app.js
+++ app.js
@@ -5,17 +5,32 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+
 let values = [];
 const PORT = 3000;
 
 
 //Launch Home page on Init
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`)
+    res.render('home');
 })
 
 //Send to Submitted page and populate array with values filled in.
 app.post("/submitted", (req, res) =>{
+
+    if(req.body.fname == "" || req.body.lname == "" || req.body.email == "")
+    {
+        res.send(
+            `
+            <h1> Invalid, please enter vaild entries </h1> 
+            <br>
+            <a href="/" class="button-style"> Back to Home </a>
+            `
+        )
+        return;
+    }
+
     values.push(req.body)
     res.send(
         `<form action="home" method="get"> 
@@ -23,39 +38,56 @@ app.post("/submitted", (req, res) =>{
             <button class="button-style" href="#" role="button"> Back to home Page </button>
         </form>`
       );
+
+    
 })
 
 app.get('/submitted', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/submitted.html`)
+    res.render("submitted.ejs")
 })
 
 
 
 //Send BACK to home page from submitted page
 app.get('/home', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`)
+    res.render("home.ejs")
 })
 
 //Dynamically generate admin page for viewing users who filled in data.
-app.get("/admin/contacts", (req, res) => {
+app.get("/admin-contacts", (req, res) => {
 
-    let applyToPage = "";
+    if(values == "")
+    {
+        res.send(`
+            <h1> Admin Contacts </h1>
+            <p> Nothing to display yet! </p>
+            <br>
+            <a href="/" class="button-style"> Back to Home </a> 
+            `);
+        return;
+    }
+
+    let applyToPage = `<h1> Admin Contacts </h1>`;
+    let date = new Date();
     for(let items of values)
     {
-      applyToPage += 
-      `<p><strong>Name:</strong> ${items.fname} ${items.lname}
-       <br>
-        <p><strong>Job Title:</strong> ${items.status}
-        <p><strong>Company:</strong> ${items.company}
-        <p><strong>How We met:</strong> ${items.Introduced}
-        <p><strong>Other-not-Specified/More-Detail:</strong> ${items.other}
-        <p><strong>Email:</strong> ${items.email}
-        <p><strong>LinkedIn:</strong> ${items.linkedIn}`;
+        applyToPage += '<div>';
+        for (let [key, value] of Object.entries(items))
+            {
+              let format = key;
+              applyToPage += `<p><strong>${format}:</strong> ${value}</p>`;
+            }
+        
+        applyToPage += `<p><strong> Date Submitted: </strong> ${date} </p>`;
+        applyToPage += "</div>";
     }
+
+    applyToPage += `<br> <a href="/" class="button-style"> Back to Home </a>`;
   
     res.send(applyToPage);
   });
 
+ 
 
 
 app.listen(PORT, () => {
